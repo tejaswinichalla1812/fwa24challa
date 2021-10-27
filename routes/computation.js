da1fca46591e001146ae8be9a29d9d9169d11dc0
tejaswinichalla1812 <tejaswini.challa12@gmail.com>
2021-10-27 18:42:21 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -6,8 +6,7 @@ router.get('/',(req,res)=>{
     let ran=Math.floor(Math.random()*100)+1;
     let data=req.query.x;
     console.log(data,ran);
-    res.send('Math.imul('+ran+') is:'+Math.imul(ran)+'Math.log('+ran+') is:'+Math.log(ran)+'Math.log10('+ran+') is:'+Math.log10(ran)+
-    'Math.trunc('+ran+') is:'+Math.trunc(ran)
+    res.send('Math.imul('+ran+') is:'+Math.imul(ran)+'Math.log('+ran+') is:'+Math.log(ran)+'Math.log10('+ran+') is:'+Math.log10(ran)
 
     );
 })
--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TEJASWINI' });
+  res.render('mydata', { title: 'TEJASWINI' });
 });
 
 module.exports = router;
