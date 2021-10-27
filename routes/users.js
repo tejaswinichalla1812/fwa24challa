9b30f3335419f169f6c1f15d89a04d1c02a271c6
tejaswinichalla1812 <tejaswini.challa12@gmail.com>
2021-10-27 15:58:14 -0500

    files to repo

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+var value=0;
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value++;
+  res.send('User accesses are: '+value);
+  console.log(value)
 });
 
 module.exports = router;
